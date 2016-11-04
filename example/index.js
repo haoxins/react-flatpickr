9be5524d16396ad95fa538271d473885ec0cd35c
haoxin <haoxins@outlook.com>
2016-11-04 14:06:11 +0800

    v1.1.0 - support controlled component
    closes #3

--- example/index.js
+++ example/index.js
@@ -7,10 +7,31 @@ import { render } from 'react-dom'
 import Flatpickr from '../'
 
 class App extends Component {
+  state = {
+    v: '2016-01-01 01:01'
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      const { v } = this.state
+      this.setState({
+        v: v.replace('2016', '2017')
+      })
+    }, 2000)
+  }
+
   render() {
+    const { v } = this.state
+
     return (
-      <Flatpickr data-enable-time className='test'
-        onChange={(obj, str, ins) => console.info(obj, str, ins)} />
+      <main>
+        <Flatpickr data-enable-time className='test'
+          onChange={(obj, str, ins) => console.info(obj, str, ins)} />
+        <Flatpickr data-enable-time defaultValue='2016-11-11 11:11'
+          onChange={(obj, str, ins) => console.info(obj, str, ins)} />
+        <Flatpickr data-enable-time value={v}
+          onChange={(obj, str, ins) => console.info(obj, str, ins)} />
+      </main>
     )
   }
 }
