120acf3598af5e3d946b2124ebe5eebd0ecec07f
landvibe <landvibe@landvibeui-iMac.local>
2016-11-29 18:43:11 +0900

    move onChange from options to props in example

--- example/index.js
+++ example/index.js
@@ -26,13 +26,13 @@ class App extends Component {
     return (
       <main>
         <Flatpickr data-enable-time className='test'
-          options={{onChange: (_, str) => console.info(str)}} />
+          onChange={(_, str) => console.info(str)} />
         <Flatpickr data-enable-time defaultValue='2016-11-11 11:11'
-          options={{onChange: (_, str) => console.info(str)}} />
-        <Flatpickr data-enable-time
-          options={{value: v, onChange: (_, str) => console.info(str)}} />
-        <Flatpickr
-          options={{value: v, minDate: "2016-11-01", onChange: (_, str) => console.info(str)}} />
+          onChange={(_, str) => console.info(str)} />
+        <Flatpickr data-enable-time options={{value: v}}
+          onChange={(_, str) => console.info(str)} />
+        <Flatpickr options={{value: v, minDate: "2016-11-01"}}
+          onChange={(_, str) => console.info(str)} />
       </main>
     )
   }
