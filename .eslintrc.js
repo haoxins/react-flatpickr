fe2cae3e4b3357a9f44f4d4eedd4f53513e15329
Emil Ajdyna <2088208+emilos@users.noreply.github.com>
2019-05-06 13:42:11 +0200

    Update dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  parser: "babel-eslint"
+}
