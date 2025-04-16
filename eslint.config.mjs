33b0649c557fbbfba84a9bdb2c86b597f6f0542c
Jason <jason.young@evite.com>
2025-04-16 11:58:25 -0700

    fixing merge conflicts, config update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,20 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import reactHooks from 'eslint-plugin-ts-react-hooks'
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-ts-react-hooks';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  {ignores: ['build']},
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended, reactHooks.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
-    plugins: {
-    },
+    plugins: {},
     rules: {
-      "@typescript-eslint/no-explicit-any": "off"
-    },
-  },
-)
-
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  }
+);
